e4d258ccc7a6620f1f90a004ddc6a62e0fe4cc23
Henrique Moraes <hrm8@duke.edu>
2013-12-05 23:07:52 -0500

    desabstractalization of cache

--- src/dblockcache/DBufferCache.java
+++ src/dblockcache/DBufferCache.java
@@ -2,11 +2,11 @@ package dblockcache;
 
 import common.Constants;
 
-public abstract class DBufferCache {
+public class DBufferCache {
 	
 	private int _cacheSize;
 	
-	/*
+	/**
 	 * Constructor: allocates a cacheSize number of cache blocks, each
 	 * containing BLOCK-size bytes data, in memory
 	 */
@@ -14,19 +14,27 @@ public abstract class DBufferCache {
 		_cacheSize = cacheSize * Constants.BLOCK_SIZE;
 	}
 	
-	/*
+	/**
 	 * Get buffer for block specified by blockID. The buffer is "held" until the
 	 * caller releases it. A "held" buffer cannot be evicted: its block ID
 	 * cannot change.
 	 */
-	public abstract DBuffer getBlock(int blockID);
+	public DBuffer getBlock(int blockID) {
+	    
+	}
 
-	/* Release the buffer so that others waiting on it can use it */
-	public abstract void releaseBlock(DBuffer buf);
+	/**
+	 *  Release the buffer so that others waiting on it can use it 
+	 */
+	public void releaseBlock(DBuffer buf) {
+	    
+	}
 	
-	/*
+	/**
 	 * sync() writes back all dirty blocks to the volume and wait for completion.
 	 * The sync() method should maintain clean block copies in DBufferCache.
 	 */
-	public abstract void sync();
+	public void sync() {
+	    
+	}
 }
